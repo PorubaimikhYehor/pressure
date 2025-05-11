f53bc7e26c75b99f0086bb11ecae85b283866903
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2025-05-11 17:21:21 +0300

    install angular, material, tailwind

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatCardModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'pressure';
+}
